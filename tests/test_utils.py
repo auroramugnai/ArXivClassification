586008ee2cc5c4d10c7083accfd2710dc6b9cfc9
Chiara Tocchini <122037548+chitocchi@users.noreply.github.com>
2024-05-01 20:46:34 +0200

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,15 @@
 import re
 
-import en_core_web_sm
-import spacy
+# import en_core_web_sm
+# import spacy
 
-from arXiv_classification.utils import remove
+from arXiv_classification.utils_prova import remove
 
 def test_remove():
     """Unit test for the remove() function.
     """
-    nlp = spacy.load("en_core_web_sm")
-    assert remove('ciao_3', nlp) == 'ciao'
-    assert remove('mi chiamo Chiara! Tu come ti chiami?', nlp) == 'mi chiamo chiara tu come ti chiami'
-    assert remove('prova%$ https:/sito.com', nlp) == 'prova'
+    # nlp = spacy.load("en_core_web_sm")
+    assert remove('ciao_3') == 'ciao'
+    assert remove('mi chiamo Chiara! Tu come ti chiami?') == 'mi chiamo chiara tu come ti chiami'
+    assert remove('prova%$ https:/sito.com') == 'prova'
 
