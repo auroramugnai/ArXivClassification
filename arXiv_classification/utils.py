c7f1329b224401092de0995e3017818320f2d811
chitocchi <tocchinichiara29@gmail.com>
2024-05-02 16:16:03 +0200

    Commit tests

--- arXiv_classification/utils.py
+++ arXiv_classification/utils.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from math import ceil
 import re
-from __future__ import annotations
+from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
 import numpy as np
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,7 +10,7 @@ def test_remove():
     """Unit test for the remove() function.
     """
     nlp = spacy.load("en_core_web_md")
-    assert remove('hi_3 !? Hi, 374189 h5i \math{hi} hi%& https:/website.com GitHub.com re-move\n', nlp) == 'hi'
+    assert remove('hi_3 !? Hi, 374189 h5i math{hi} hi%& https:/website.com GitHub.com hi-\n', nlp) == 'hi'
 
 
 def test_is_string_series():
