af5eca68ae0d9be86890b45d948c97493b2972f1
Chiara Tocchini <122037548+chitocchi@users.noreply.github.com>
2024-05-02 15:54:50 +0200

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,4 @@ def test_remove():
     """Unit test for the remove() function.
     """
     nlp = spacy.load("en_core_web_md")
-    assert remove('ciao_3 ciao', nlp) == 'ciao'
-    assert remove('mi chiamo Chiara! Tu come ti chiami?', nlp) == 'mi chiamo chiara tu come ti chiami'
-    assert remove('prova%$ https:/sito.com prova', nlp) == 'prova'
+    assert remove('hi_3 !? Hi, 374189 h5i \math{hi} hi%& https:/website.com GitHub.com re-move\n', nlp) == 'hi'
