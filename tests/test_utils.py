e308642e4d9ce3c8a73085b6124b3ef225b5b5af
Chiara Tocchini <122037548+chitocchi@users.noreply.github.com>
2024-05-03 11:04:53 +0200

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,15 +17,15 @@ def test_text_cleaner():
 def test_is_string_series():
     """Unit test for is_string_series() function.
     """
-    # d1 = {'a': 'This', 'b': 'is', 'c': 'Python'}
-    # series1 = pd.Series(data=d1, index=['a', 'b', 'c'])
+    d1 = {'a': 'This', 'b': 'is', 'c': 'Python'}
+    series1 = pd.Series(data=d1, index=['a', 'b', 'c'])
     d2 = {'a': 'My age', 'b': 'is', 'c': 23}
     series2 = pd.Series(data=d2, index=['a', 'b', 'c'])
     d3 = {'a': ['Hello', 'my'], 'b': ['name', 'is'], 'c': ['Pippo', '!']}
     series3 = pd.Series(data=d3, index=['a', 'b', 'c'])    
     d4 = {'a': "['Hello', 'my']", 'b': "['name', 'is']", 'c': "['Pippo', '!']"}
     series4 = pd.Series(data=d4, index=['a', 'b', 'c'])    
-    # assert is_string_series(series1) == True
+    assert is_string_series(series1) == True
     assert is_string_series(series2) == False
     assert is_string_series(series3) == False
     assert is_string_series(series4) == True
