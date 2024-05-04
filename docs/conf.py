d8a5a5cb61381f2c675987d2bc94ff71060cca2a
chitocchi <tocchinichiara29@gmail.com>
2024-05-04 18:22:58 +0200

    add documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,17 +19,9 @@ release = '0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
-]
-
-autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'undoc-members': True,
-    'private-members': True
-}
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.napoleon', 
+              'sphinx.ext.viewcode']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -34,5 +31,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,18 @@
 .. arXiv_classification documentation master file, created by
-   sphinx-quickstart on Fri May  3 15:39:29 2024.
+   sphinx-quickstart on Sat May  4 18:07:17 2024.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
 Welcome to arXiv_classification's documentation!
 ================================================
 
+Contents:
+
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
-.. automodule:: utils
-   :members:
+   modules
+
 
 
 Indices and tables
@@ -20,3 +21,4 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
