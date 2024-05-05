8fb03fe38f3a41333002eb2ac4cd9913a9a1938f
Chiara Tocchini <122037548+chitocchi@users.noreply.github.com>
2024-05-05 19:17:08 +0200

    Delete set_up.sh

--- set_up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git remote remove origin
-git remote add origin https://github.com/RGGH/jubilant-lamp.git
-pip install Sphinx
-pip install sphinx sphinx_rtd_theme
-touch Docs
-cd Docs
-sphinx-quickstart
