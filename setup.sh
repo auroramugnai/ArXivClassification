e7173251d4c38fa21ad01050d49c84ab29c86d75
Aurora Mugnai <146384704+auroramugnai@users.noreply.github.com>
2024-05-06 22:14:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git remote remove origin
-git remote add origin https://github.com/RGGH/jubilant-lamp.git
+git remote add origin https://github.com/auroramugnai/ArXivClassification.git
 pip install Sphinx
 pip install sphinx sphinx_rtd_theme
 touch Docs
