0cf14c9731a86ac6463b687f55376a99df7849c1
Chiara Tocchini <122037548+chitocchi@users.noreply.github.com>
2024-05-06 10:08:04 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,8 @@ release = '0.0'
 
 extensions = ['sphinx.ext.autodoc', 
               'sphinx.ext.napoleon', 
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
