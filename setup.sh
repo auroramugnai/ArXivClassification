8e33da0cadce50b59d1e11404800e2b78b19b5fa
Aurora Mugnai <146384704+auroramugnai@users.noreply.github.com>
2024-05-07 10:11:05 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git remote remove origin
-git remote add origin https://github.com/auroramugnai/ArXivClassification.git
-pip install Sphinx
-pip install sphinx sphinx_rtd_theme
-touch Docs
-cd Docs
-sphinx-quickstart
